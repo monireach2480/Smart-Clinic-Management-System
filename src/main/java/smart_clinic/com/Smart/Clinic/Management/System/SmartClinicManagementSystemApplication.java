ba48b90fc9029d91adc8819c3b29d6d2ec16467d
Len Monireach <lenmonireach123@gmail.com>
2025-06-01 13:43:15 +0700

    Set Up Spring Boot

---
+++ src/main/java/smart_clinic/com/Smart/Clinic/Management/System/SmartClinicManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package smart_clinic.com.Smart.Clinic.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartClinicManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartClinicManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/smart_clinic/com/Smart/Clinic/Management/System/SmartClinicManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package smart_clinic.com.Smart.Clinic.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartClinicManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
